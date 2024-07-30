9ba931107f8cc6cdb85dde31e5fbe06b5c50d45e
Haky86 <hakamelassouad@gmail.com>
2024-07-30 12:05:03 +0200

    a71: Create dummy sepolicy template
    Change-Id: I701fc9c83495443160c4ab3a60e470b778529552

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,5 +23,8 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Security Patch Level
 VENDOR_SECURITY_PATCH := 2024-02-01
 
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Get non-open-source specific aspects
 include vendor/samsung/a71/BoardConfigVendor.mk
